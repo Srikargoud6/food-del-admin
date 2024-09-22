f01567b1cf1ab5b1f79cf1915474708e4a3b2ee0
Srikargoud6 <srikargoud611@gmail.com>
2024-09-22 14:56:57 +0530

    added a page to list all the food items in the db

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const url = "http://localhost:4000";
+
   return (
     <div>
       <ToastContainer />
@@ -17,8 +19,8 @@ function App() {
       <div className="d-flex">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add />} />
-          <Route path="/list" element={<List />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
           <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,8 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./List.css";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ url }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  });
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Category</th>
+            <th scope="col">Price</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th>
+                  <img
+                    src={`${url}/images/` + item.image}
+                    alt="image"
+                    srcset=""
+                    className="food-img col-margin"
+                  />
+                </th>
+                <th scope="row">{item.name}</th>
+                <td>{item.category}</td>
+                <td>${item.price}</td>
+                <td className="cross-icon" onClick={() => removeFood(item._id)}>
+                  x
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default List;
